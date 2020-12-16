00f10b7566aa77b57f70da4e5064bb49a43f24a5
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-12-16 16:08:34 -0500

    added error page for other routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import ErrorPage from "./pages/ErrorPage";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
@@ -11,6 +12,7 @@ function App() {
       <div>
         <Wrapper>
           <Route exact path="/" component={MainPage}/>
+          {/* <Route exact path="/*" component={ErrorPage}/> */}
           </Wrapper>
         <Footer />
       </div>
